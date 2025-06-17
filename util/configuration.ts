af5349d8103567cbcd2c18e47f2741a7a1a953c3
Rémi ALBAN <remi.alban@insa-cvl.fr>
2025-06-17 12:30:05 +0200

    Add configuration class

---
+++ util/configuration.ts
@@ -0,0 +1,30 @@
+import {TerraNode} from "@/util/types";
+
+class Configuration extends TerraNode {
+    private nodes: TerraNode[] = [];
+
+    public addNode(node: TerraNode) {
+        this.nodes.push(node);
+    }
+
+    public removeNode(node: TerraNode) {
+        this.nodes = this.nodes.filter(n => n !== node);
+    }
+
+    generateConfigNode(): string {
+        return `
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.78.2"
+    }
+  }
+}
+        `;
+    }
+
+    getChildren(): TerraNode[] {
+        return this.nodes;
+    }
+}
