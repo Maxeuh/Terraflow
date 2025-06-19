7b21b99094fdd0a3ff2299c791e33c2cc4f03101
Maxence <maxencekrill@gmail.com>
2025-06-19 23:22:49 +0200

    refactor(/about): enhance contributor display using Mantine and remove ImageWithText component
    * Added loading overlay and error handling for contributor fetching.
    * Improved layout with Mantine components for better UI.
    * Removed unused ImageWithText component to streamline code.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,65 +1,135 @@
-'use client';
+"use client";
 
+import {
+    Badge,
+    Button,
+    Card,
+    Container,
+    Grid,
+    Group,
+    Image,
+    LoadingOverlay,
+    Stack,
+    Text,
+    Title,
+} from "@mantine/core";
 import { useEffect, useState } from "react";
-import ImageWithText from "@/components/ImageWithText/ImageWithText";
-import { Button } from "@mantine/core";
 
 interface Contributor {
-  login: string;
-  id: number;
-  avatar_url: string;
-  html_url: string;
-  contributions: number;
+    login: string;
+    id: number;
+    avatar_url: string;
+    html_url: string;
+    contributions: number;
 }
 
 export default function Home() {
-  const [contributors, setContributors] = useState<Contributor[]>([]);
-  const [error, setError] = useState<string | null>(null);
+    const [contributors, setContributors] = useState<Contributor[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchContributors = async () => {
-      try {
-        const response = await fetch("/api/contributors");
-        const data: Contributor[] = await response.json();
-        setContributors(data);
-      } catch (err: unknown) {
-        if (err instanceof Error) {
-          console.error(err.message);
-          setError(err.message || "Échec du chargement des contributeurs");
-        } else if (typeof err === "string") {
-          console.error(err);
-          setError(err);
-        }
-      }
-    };
+    useEffect(() => {
+        const fetchContributors = async () => {
+            try {
+                setLoading(true);
+                const response = await fetch("/api/contributors");
+                const data: Contributor[] = await response.json();
+                setContributors(data);
+            } catch (err: unknown) {
+                if (err instanceof Error) {
+                    console.error(err.message);
+                    setError(
+                        err.message ||
+                            "An error occurred while fetching contributors."
+                    );
+                } else if (typeof err === "string") {
+                    console.error(err);
+                    setError(err);
+                }
+            } finally {
+                setLoading(false);
+            }
+        };
 
-    fetchContributors();
-  }, []);
+        fetchContributors();
+    }, []);
 
-  return (
+    return (
+        <>
+            <LoadingOverlay visible={loading} />
 
+            <Container size="md" py="xl">
+                <Stack align="center" gap="xl">
+                    <Title order={1} mt="lg">
+                        About the project
+                    </Title>
 
+                    {error && <Text c="red">{error}</Text>}
 
-    <div className="min-h-screen bg-gray-100">
+                    <Grid w="100%" gutter="md">
+                        {contributors.map((contributor) => (
+                            <Grid.Col
+                                key={contributor.id}
+                                span={{ base: 12, xs: 6, sm: 4, md: 3 }}
+                            >
+                                <Card
+                                    shadow="sm"
+                                    padding="lg"
+                                    radius="md"
+                                    withBorder
+                                >
+                                    <Card.Section>
+                                        <Image
+                                            src={contributor.avatar_url}
+                                            height={100}
+                                            alt={contributor.login}
+                                        />
+                                    </Card.Section>
 
-        <div className="min-h-screen flex flex-col items-center justify-center space-y-8 py-10">
+                                    <Group
+                                        justify="space-between"
+                                        mt="md"
+                                        mb="xs"
+                                    >
+                                        <Text size="lg" fw={800}>
+                                            {contributor.login}
+                                        </Text>
+                                        <Badge>
+                                            {`${contributor.contributions} ${
+                                                contributor.contributions > 1
+                                                    ? "contributions"
+                                                    : "contribution"
+                                            }`}
+                                        </Badge>
+                                    </Group>
 
-        <h1 className="mt-8 text-xl font-bold text-gray-900 text-center"> About the project </h1>
+                                    <Button
+                                        component="a"
+                                        href={contributor.html_url}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        fullWidth
+                                        mt="md"
+                                        radius="md"
+                                    >
+                                        View Profile
+                                    </Button>
+                                </Card>
+                            </Grid.Col>
+                        ))}
+                    </Grid>
 
-        {contributors.map((contributor) => (
-      <ImageWithText
-        lien = {contributor.html_url}
-        imageUrl= {contributor.avatar_url}
-        altText={contributor.login}
-        maincaption={contributor.login}
-        
-        secondarycaption= { ` ${contributor.contributions} ${contributor.contributions > 1?"contributions":"contribution"}`}
-      />
-            ))}
-        <Button component="a" href="https://github.com/Maxeuh/Terraflow" variant="filled">Contribute on GitHub</Button>
-    </div>
-
-    </div>
-  );
+                    <Button
+                        component="a"
+                        href="https://github.com/Maxeuh/Terraflow"
+                        variant="filled"
+                        size="md"
+                        radius="md"
+                    >
+                        Contribute on GitHub
+                    </Button>
+                </Stack>
+            </Container>
+        </>
+    );
 }
-
